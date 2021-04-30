a7e56d7ee4f5f83dca22592b7dec116ce0feb902
Natnael Eshetu <the.defalt8@gmail.com>
2021-04-30 09:50:11 +0300

    Declared integer types

--- include/axl.util/types.hpp
+++ include/axl.util/types.hpp
@@ -4,6 +4,15 @@ namespace axl {
 namespace util {
 
 typedef unsigned int size_t;
+typedef unsigned char byte;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+typedef unsigned long long uint64;
+typedef char int8;
+typedef short int16;
+typedef long int32;
+typedef long long int64;
 
 } // axl::util
 } // axl
